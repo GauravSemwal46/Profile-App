170e9eaf91cf7ebb5e268bf3d82aa2edd82c6e99
GauravSemwal46 <gauravsemwal6@gmail.com>
2025-03-03 15:03:27 +0530

    Create UI for the profile app

---
+++ ProfileApp/ProfileApp/ProfileAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ProfileAppApp.swift
+//  ProfileApp
+//
+//  Created by Gaurav Semwal on 03/03/25.
+//
+
+import SwiftUI
+
+@main
+struct ProfileAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
